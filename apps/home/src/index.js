79e7aea1ce7dafc8427732d6e828fa77b24b82b9
dfloren <daniel_florendo1@yahoo.com>
2023-06-12 22:59:55 -0400

    feat: projects section

--- apps/home/src/index.js
+++ apps/home/src/index.js
@@ -12,6 +12,17 @@ function createStarGifImg() {
     return img;
 }
 
+function createSlidingPointerImg() {
+    let rightArrowImg = document.createElement("img");
+    rightArrowImg.classList.add("slide-left-right");
+    rightArrowImg.src = "../resources/images/right-arrow.svg";
+    rightArrowImg.alt = "yellow right arrow";
+    rightArrowImg.style.width = "25px";
+    rightArrowImg.style.paddingLeft = "5px";
+
+    return rightArrowImg;
+}
+
 (function initRootContainer() {
     let rootContainer = document.getElementById("root-container");
     rootContainer.classList.add("font-noto-sans-japanese");
@@ -27,7 +38,7 @@ function createStarGifImg() {
     introDescriptionSpan.innerHTML = "&nbsp;" + content.CONTENT_INTRO_DESC;
 
     let introHeaderDiv = document.createElement("div");
-    introHeaderDiv.classList.add("width-full", "margin-20px");
+    introHeaderDiv.classList.add("padding-20px");
     introHeaderDiv.append(nameStrong);
     introHeaderDiv.append(introDescriptionSpan);
 
@@ -36,11 +47,11 @@ function createStarGifImg() {
     introShortDescSpan.innerText = content.CONTENT_INTRO_SHORT_DESC;
 
     let introShortDescDiv = document.createElement("div");
-    introShortDescDiv.classList.add("width-full", "margin-20px");
+    introShortDescDiv.classList.add("padding-20px");
     introShortDescDiv.append(introShortDescSpan);
 
     let introEmploymentCompanyAnchor = document.createElement("a");
-    introEmploymentCompanyAnchor.classList.add("heading-2", "font-ubuntu", "color-purple", "slide-right", "display-inline-block");
+    introEmploymentCompanyAnchor.classList.add("heading-2", "font-ubuntu", "color-purple", "display-inline-block", "slide-up-on-hover");
     introEmploymentCompanyAnchor.href = "https://www.capgemini.com/";
     introEmploymentCompanyAnchor.target ="_blank";
     introEmploymentCompanyAnchor.innerText = content.CONTENT_INTRO_EMPLOYMENT_COMPANY;
@@ -50,7 +61,7 @@ function createStarGifImg() {
     introEmploymentDescSpan.innerHTML = content.CONTENT_INTRO_EMPLOYMENT_DESC + "&nbsp;";
 
     let introEmploymentDiv = document.createElement("div");
-    introEmploymentDiv.classList.add("width-full", "margin-20px");
+    introEmploymentDiv.classList.add("padding-20px");
     introEmploymentDiv.append(introEmploymentDescSpan);
     introEmploymentDiv.append(introEmploymentCompanyAnchor);
 
@@ -63,31 +74,91 @@ function createStarGifImg() {
 
 (function initProjects() {
     let projectsHeaderDiv = document.createElement("div");
-    projectsHeaderDiv.classList.add("heading-1", "font-weight-300", "width-full", "section-header");
+    projectsHeaderDiv.classList.add("heading-1", "font-weight-300", "section-header");
     projectsHeaderDiv.innerText = "Projects";
 
     let projectsHeaderDescDiv = document.createElement("div");
-    projectsHeaderDescDiv.classList.add("heading-3", "font-weight-300", "width-full", "section-desc");
+    projectsHeaderDescDiv.classList.add("heading-3", "font-weight-300", "italic", "section-desc");
     projectsHeaderDescDiv.innerText = content.CONTENT_PROJ_SHORT_DESC;
 
     projectsHeaderDiv.append(projectsHeaderDescDiv);
 
-    let boggleSolverLink = document.createElement("a");
-    boggleSolverLink.href = "/apps/bogglesolver/src/index.html";
-    boggleSolverLink.target = "_blank";
-    boggleSolverLink.innerText = "Boggle Solver";
+    let boggleSolverAnchor = document.createElement("a");
+    boggleSolverAnchor.classList.add("heading-2", "display-inline-block", "color-purple", "yellow-on-hover");
+    boggleSolverAnchor.href = "/apps/bogglesolver/src/index.html";
+    boggleSolverAnchor.target = "_blank";
+    boggleSolverAnchor.innerText = "Boggle Solver";
+    
+    boggleSolverAnchor.append(createSlidingPointerImg());
 
-    let coinstashLink = document.createElement("a");
-    coinstashLink.href = "https://github.com/dfloren/CoinStash-public";
-    coinstashLink.target = "_blank";
-    coinstashLink.innerText = "Coin Stash";
+    let boggleHeaderDiv = document.createElement("div");
+    boggleHeaderDiv.classList.add("section-header");
+    boggleHeaderDiv.append(boggleSolverAnchor);
+
+    let boggleHeaderDescDiv = document.createElement("div");
+    boggleHeaderDescDiv.classList.add("font-weight-300", "section-desc");
+    boggleHeaderDescDiv.innerText = "A solver for the super noisy word game Boggle.";
+
+    let boggleSolverVideo = document.createElement("video");
+    boggleSolverVideo.classList.add("padding-20px");
+    boggleSolverVideo.toggleAttribute("muted", true);
+    boggleSolverVideo.toggleAttribute("playsinline", true);
+    boggleSolverVideo.toggleAttribute("controls", true);
+    boggleSolverVideo.toggleAttribute("loop", true);
+    boggleSolverVideo.toggleAttribute("autoplay", true);
+
+    let boggleSolverVideoSource = document.createElement("source");
+    boggleSolverVideoSource.src = "../resources/videos/bogglesolverdemo.mp4";
+    boggleSolverVideoSource.type = "video/mp4";
+
+    boggleSolverVideo.append(boggleSolverVideoSource);
+
+    let boggleSolverVideoDiv = document.createElement("div");
+    boggleSolverVideoDiv.append(boggleSolverVideo);
+
+    let coinStashAnchor = document.createElement("a");
+    coinStashAnchor.classList.add("heading-2", "display-inline-block", "color-purple", "yellow-on-hover");
+    coinStashAnchor.href = "https://github.com/dfloren/CoinStash-public";
+    coinStashAnchor.target = "_blank";
+    coinStashAnchor.innerText = "Coin Stash";
+
+    coinStashAnchor.append(createSlidingPointerImg());
+
+    let coinStashHeaderDiv = document.createElement("div");
+    coinStashHeaderDiv.classList.add("section-header");
+    coinStashHeaderDiv.append(coinStashAnchor);
+
+    let coinStashHeaderDescDiv = document.createElement("div");
+    coinStashHeaderDescDiv.classList.add("font-weight-300", "section-desc");
+    coinStashHeaderDescDiv.innerText = "A cryptocurrency portfolio management system built with Django.";
+
+    let coinStashVideo = document.createElement("video");
+    coinStashVideo.classList.add("padding-20px");
+    coinStashVideo.toggleAttribute("muted", true);
+    coinStashVideo.toggleAttribute("playsinline", true);
+    coinStashVideo.toggleAttribute("controls", true);
+    coinStashVideo.toggleAttribute("loop", true);
+    coinStashVideo.toggleAttribute("autoplay", true);
+
+    let coinStashVideoSource = document.createElement("source");
+    coinStashVideoSource.src = "../resources/videos/coinstashdemo.mp4";
+    coinStashVideoSource.type = "video/mp4";
+
+    coinStashVideo.append(coinStashVideoSource);
 
     let projects = document.getElementById("projects-container");
     projects.classList.add("section-container");
     projects.append(projectsHeaderDiv);
     projects.append(projectsHeaderDescDiv);
-    projects.append(boggleSolverLink);
-    projects.append(coinstashLink);
+
+    projects.append(boggleHeaderDiv);
+    projects.append(boggleHeaderDescDiv);
+    projects.append(boggleSolverVideoDiv);
+
+    projects.append(coinStashHeaderDiv)
+    projects.append(coinStashHeaderDescDiv);
+    projects.append(coinStashVideo);
+
 })();
 
 
--- apps/home/src/scripts/utilities/content.js
+++ apps/home/src/scripts/utilities/content.js
@@ -1,8 +1,8 @@
 export const CONTENT_INTRO_NAME = "Daniel";
-export const CONTENT_INTRO_DESC = `is a computer engineer with an interest in all things related to full stack development. He has professional experience building backend 
-components, and dabbles with frontend technologies in his free time.`;
+export const CONTENT_INTRO_DESC = `is a computer engineer with an interest in all things related to full stack development. He has professional experience building backend
+ components including API's and ETL pipelines.` 
 export const CONTENT_INTRO_SHORT_DESC = "Software developer, amateur photographer, keeb enthusiast.";
 export const CONTENT_INTRO_EMPLOYMENT_DESC = "Currently building cool stuff at";
 export const CONTENT_INTRO_EMPLOYMENT_COMPANY = "Capgemini.";
 
-export const CONTENT_PROJ_SHORT_DESC = "A collection of random things I've built while learning various tech.";
+export const CONTENT_PROJ_SHORT_DESC = "A collection of random things I've built, try them out!";
